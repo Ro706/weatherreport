958782077837033509598a23372723b4f971d9c8
Rohit Mandal <60247178+Ro706@users.noreply.github.com>
2021-03-26 09:39:17 +0530

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,21 @@
+from pyfiglet import Figlet
+import requests
+import json
+f = Figlet(font='slant')
+print (f.renderText('weather report'))
+City = str(input('enter your city name: '))
+api_key ='04018081b69cca6f721c5ed1a46be071'
+base_url = 'https://api.openweathermap.org/data/2.5/weather?'
+url = base_url+'appid='+api_key+'&q='+City
+response = requests.get(url)
+x=response.json()
+print (x)
+if x['cod']!=401:
+    print ('city name:',x['name'])
+    print ('weather:',x['weather'])
+    print ('weather:',x['weather'][0]['main'])
+    print ('temp:',x['main']['temp'])
+    print ('minimum temp:',x['main']['temp_min'])
+    print ('max temp:',x['main']['temp_max'])
+else:
+    print ('city not found')
